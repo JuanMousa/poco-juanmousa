0439d0b0624d82824d5c297ab4b5ea38acc55151
JaunMousa <juan.mousa@powercoders.org>
2024-04-25 20:15:03 +0200

    javascript excersice week4

---
+++ javascript-excersice/week4/script.js
@@ -0,0 +1,92 @@
+
+// let birt = document.getElementById("birt").value;
+// let newBirt = new Date(birt).getFullYear();
+
+// let todayDate = document.getElementById("todayDate").value;
+// let newTodayDate = new Date(todayDate).getFullYear();
+
+// let resentAge = document.getElementById("resentAge").value;
+// let age = new Date(resentAge);
+// let claulate = document.getElementById("claulate").value
+
+
+
+// the age calculter !important 
+// function yourAge(){
+//     let birt = document.getElementById("birt").value;
+//     let newBirt = new Date(birt).getFullYear();
+
+//     let todayDate = document.getElementById("todayDate").value;
+//     let newTodayDate = new Date(todayDate).getFullYear();
+//     let age = newTodayDate - newBirt;
+//     document.getElementById("resentAge"). textContent = `your age is ${age}`;
+//     let claulate = document.getElementById("claulate").value
+// }
+
+
+
+
+// let weekDays = ["monday", "tusday", "wednsday", "thursday", "friday", "saturday", "sunday"];
+
+// let wochenTag = ["montag", "dinstag", "mittwoch", "Donerstag", "Fritag", "samstag", "sontag"]
+
+// for (let i = 0; i < weekDays.length; i++) {
+//     weekDays[i] == wochenTag[0] 
+//     console.log(weekDays[i]);
+// }
+// if (weekDays[0] = "monday") {
+//     console.log("Montag");
+// }
+// if (weekDays[1] = "tusday") {
+//     console.log("Dienstag")
+// }
+// if (weekDays[2] = "wednsday") {
+//     console.log("Mittwoch")
+// }
+// if (weekDays[3] = "thursday") {
+//     console.log("Donerstag")
+// }
+// if (weekDays[4] = "friday") {
+//     console.log("Fritag")
+// }
+// if (weekDays[5] = "saturday") {
+//     console.log("Samstag")
+// }
+// if (weekDays[6] = "sunday") {
+//     console.log("Sontag")
+// }
+
+
+
+
+//  the ODD and Even Excrsice
+// let peropm = prompt("please put a number betwen 0 and 20");
+// console.log(peropm)
+
+// let inter = peropm / 2 // 12-2-2-2-2-2
+// console.log(inter)
+
+// if (inter == 0) {
+//     console.log("Even")
+// } else {
+//     console.log("ODD")
+// };
+
+
+// Odd / even reporter 2.0 excrsice
+// for (let i = 0; i < 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log("Even")
+//     } else {
+//         console.log("ODD")
+//     }
+// }
+
+
+// Multiplication tables excersice
+// let prom = prompt("please add a number between 0 and 10");
+// console.log(prom * 2);
+
+// for (let i = 0; i < 11; i++) {
+//     console.log(i * 2);
+// }
