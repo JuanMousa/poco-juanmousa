fd42f3cc66e840835313209f85b84851dc07be83
JaunMousa <juan.mousa@powercoders.org>
2024-05-01 14:00:08 +0200

    simple number comrmising

---
+++ javascript-excersice/numbers-compramising/scripts.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 10;
+
+if (a >= b) {
+    console.log(`The greater number of ${a} and ${b} is ${a}`);
+} else {
+    console.log(`The smaller number of ${a} and ${b} is ${b}`);
+};
